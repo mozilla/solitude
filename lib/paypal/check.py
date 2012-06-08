8fa97a1e81b6721cf77e2555f75e9cb88bd73d10
Andy McKay <amckay@mozilla.com>
2012-06-08 16:49:38 -0700

    add in check

---
+++ lib/paypal/check.py
@@ -0,0 +1,143 @@
+import hashlib
+import logging
+import uuid
+
+from .client import Client
+from .errors import PaypalError
+
+log = logging.getLogger('s.paypal')
+
+
+class Check(object):
+    """
+    Run a series of tests on PayPal for either an addon or a paypal_id.
+    The add-on is not required, but we'll do another check or two if the
+    add-on is there.
+    """
+
+    def __init__(self, paypal_id=None, paypal_permissions_token=None,
+                 prices=None):
+        # If this state flips to False, it means they need to
+        # go to Paypal and re-set up permissions. We'll assume the best.
+        self.state = {'permissions': True}
+        self.tests = ['id', 'refund', 'currencies']
+        for test in self.tests:
+            # Three states for pass:
+            #   None: haven't tried
+            #   False: tried but failed
+            #   True: tried and passed
+            self.state[test] = {'pass': None, 'errors': []}
+        self.paypal_id = paypal_id
+        self.paypal_permissions_token = paypal_permissions_token
+        self.prices = prices
+        self.paypal = Client()
+
+    def all(self):
+        self.check_id()
+        self.check_refund()
+        self.check_currencies()
+
+    def failure(self, test, msg):
+        self.state[test]['errors'].append(msg)
+        self.state[test]['pass'] = False
+
+    def pass_(self, test):
+        self.state[test]['pass'] = True
+
+    def check_id(self):
+        """Check that the paypal id is good."""
+        test_id = 'id'
+        if not self.paypal_id:
+            self.failure(test_id, 'No PayPal id provided.')
+            return
+
+        valid, msg = self.paypal.check_paypal_id(self.paypal_id)
+        if not valid:
+            self.failure(test_id, 'You do not seem to have a PayPal account.')
+        else:
+            self.pass_(test_id)
+
+    def check_refund(self):
+        """Check that we have the refund permission."""
+        test_id = 'refund'
+        msg = ('You have not setup permissions for us to check this '
+               'paypal account.')
+
+        token = self.paypal_permissions_token
+        if not token:
+            self.state['permissions'] = False
+            self.failure(test_id, msg)
+            return
+
+        try:
+            status = self.paypal.check_permission(token, ['REFUND'])
+            if not status:
+                self.state['permissions'] = False
+                self.failure(test_id, 'No permission to do refunds.')
+            else:
+                self.pass_(test_id)
+        except PaypalError:
+            self.state['permissions'] = False
+            self.failure(test_id, msg)
+            log.info('Refund permission check returned an error '
+                     'for %s' % id, exc_info=True)
+
+    def check_currencies(self):
+        """Check that we've got the currencies."""
+        test_id = 'currencies'
+        if not self.prices:
+            self.failure(test_id, 'No prices specified.')
+            return
+
+        for currency, amount in self.prices:
+            try:
+                self.test_paykey({'currency': currency,
+                                  'amount': amount,
+                                  'email': self.paypal_id})
+                log.info('Get paykey passed in %s' % currency)
+            except PaypalError:
+                msg = 'Failed to make a test transaction in %s.' % (currency)
+                self.failure(test_id, msg)
+                log.info('Get paykey returned an error'
+                         'in %s' % currency, exc_info=True)
+
+        # If we haven't failed anything by this point, it's all good.
+        if self.state[test_id]['pass'] is None:
+            self.pass_(test_id)
+
+    def test_paykey(self, data):
+        """
+        Wraps get_paykey filling none optional data with test data. This
+        should never ever be used for real purchases.
+
+        The only things that you can set on this are:
+        email: who the money is going to (required)
+        amount: the amount of money (required)
+        currency: valid paypal currency, defaults to USD (optional)
+        """
+        data.update({
+            'pattern': '',
+            'ip': '127.0.0.1',
+            'preapproval': None,
+            'slug': 'foo',
+            'uuid': hashlib.md5(str(uuid.uuid4())).hexdigest()
+        })
+        return self.paypal.get_pay_key(data)
+
+    @property
+    def passed(self):
+        """Returns a boolean to check that all the attempted tests passed."""
+        values = [self.state[k] for k in self.tests]
+        passes = [s['pass'] for s in values if s['pass'] is not None]
+        if passes:
+            return all(passes)
+        return False
+
+    @property
+    def errors(self):
+        errs = []
+        for k in self.tests:
+            if self.state[k]['pass'] is False:
+                for err in self.state[k]['errors']:
+                    errs.append(err)
+        return errs
---
+++ lib/paypal/tests/test_check.py
@@ -0,0 +1,69 @@
+from decimal import Decimal
+
+from mock import patch
+from nose.tools import eq_
+
+from ..check import Check
+from ..client import Client
+from ..errors import PaypalError
+
+import test_utils
+
+
+class TestCheck(test_utils.TestCase):
+
+    def setUp(self):
+        self.paypal_id = 'foo@bar.com'
+        self.paypal_permissions_token = 'foo'
+        self.prices = [('USD', Decimal('1.00'))]
+        self.multiple_prices = self.prices + [('EUR', Decimal('0.5'))]
+        self.check = Check(self.paypal_id, self.paypal_permissions_token,
+                           self.prices)
+
+    def test_passed(self):
+        self.check.pass_('id')
+        assert self.check.passed, self.check.state
+
+    def test_fail(self):
+        self.check.failure('id', 'Something')
+        assert not self.check.passed, self.check.state
+
+    def test_id_none(self):
+        self.check.paypal_id = None
+        self.check.check_id()
+        assert not self.check.passed, self.check.state
+
+    @patch.object(Client, 'check_permission')
+    def test_refund(self, check_permission):
+        check_permission.return_value = {'status': True}
+        self.check.check_refund()
+        assert self.check.passed, self.check.state
+
+    @patch.object(Client, 'check_permission')
+    def test_refund_error(self, check_permission):
+        check_permission.side_effect = PaypalError
+        self.check.check_refund()
+        assert not self.check.passed, self.check.state
+
+    @patch.object(Client, 'get_pay_key')
+    def test_currency(self, get_pay_key):
+        self.check.prices = self.multiple_prices
+        self.check.check_currencies()
+        eq_(get_pay_key.call_args_list[0][0][0]['currency'], 'USD')
+        eq_(get_pay_key.call_args_list[1][0][0]['currency'], 'EUR')
+        assert self.check.passed, self.check.state
+
+    @patch.object(Client, 'get_pay_key')
+    def test_currency_fails(self, get_pay_key):
+        get_pay_key.side_effect = PaypalError()
+        self.check.check_currencies()
+        assert not self.check.passed, self.check.state
+        eq_(self.check.errors,
+            ['Failed to make a test transaction in USD.'])
+
+    def test_no_currency(self):
+        self.check.prices = None
+        self.check.check_currencies()
+        assert not self.check.passed, self.check.state
+        eq_(self.check.errors,
+            ['No prices specified.'])
