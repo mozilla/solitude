85c8ffcdc8e215cb80986e16c7dd060ed5605dcd
Jeremiah Orem <jeremy.orem@gmail.com>
2012-06-19 09:16:03 -0700

    Remove celery restart and cron generation from deploy script.

--- bin/update/deploy.py
+++ bin/update/deploy.py
@@ -59,20 +59,6 @@ def update_db(ctx):
 
 
 @task
-def install_cron(ctx):
-    """Use gen-crons.py method to install new crontab.
-
-    Ops will need to adjust this to put it in the right place.
-
-    """
-    with ctx.lcd(settings.SRC_DIR):
-        ctx.local('python2.6 ./bin/crontab/gen-crons.py -w %s -u apache > '
-                  '/etc/cron.d/.%' % (settings.WWW_DIR, settings.CRON_NAME))
-        ctx.local('mv /etc/cron.d/.%s /etc/cron.d/%s' %
-                  (settings.CRON_NAME,  settings.CRON_NAME))
-
-
-@task
 def checkin_changes(ctx):
     """Use the local, IT-written deploy script to check in changes."""
     ctx.local(settings.DEPLOY_SCRIPT)
@@ -82,14 +68,6 @@ def checkin_changes(ctx):
 def deploy_app(ctx):
     """Call the remote update script to push changes to webheads."""
     ctx.remote(settings.REMOTE_UPDATE_SCRIPT)
-    ctx.remote('/bin/touch %s' % settings.REMOTE_WSGI)
-
-
-@hostgroups(settings.CELERY_HOSTGROUP, remote_kwargs={'ssh_key': settings.SSH_KEY})
-def update_celery(ctx):
-    """Update and restart Celery."""
-    ctx.remote(settings.REMOTE_UPDATE_SCRIPT)
-    ctx.remote('/sbin/service %s restart' % settings.CELERY_SERVICE)
 
 
 @task
@@ -125,10 +103,8 @@ def update(ctx):
 
 @task
 def deploy(ctx):
-    install_cron()
     checkin_changes()
     deploy_app()
-    update_celery()
 
 
 @task
