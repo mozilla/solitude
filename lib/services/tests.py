f77c5ac8c164153c0cf20c56b17f63f490167f31
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-07-26 14:18:45 -0700

    add test for status api endpoint

---
+++ lib/services/tests.py
@@ -0,0 +1,26 @@
+import json
+
+from django.core.cache import cache
+
+from mock import patch
+from nose.tools import eq_
+
+from solitude.base import APITest
+
+
+class TestStatus(APITest):
+
+    def setUp(self):
+        self.api_name = 'services'
+        self.list_url = self.get_list_url('status')
+
+    def test_working_status(self):
+        res = self.client.get(self.list_url)
+        eq_(res.status_code, 200)
+
+    @patch.object(cache, 'get', lambda x: None)
+    def test_failure_status(self):
+        res = self.client.get(self.list_url)
+        eq_(res.status_code, 500)
+        data = json.loads(res.content)
+        eq_(data['error_message'], '<Status: database: True, cache: False>')
