defdaabcaffcccff6568b73e3753e1f1617e3bf8
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2012-10-17 05:52:18 -0700

    handle non existent uuids without exposing anything

--- lib/buyers/views.py
+++ lib/buyers/views.py
@@ -3,12 +3,15 @@ import json
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 
-from buyers.models import Buyer
+from .models import Buyer
 
 
 @require_POST
 def check_pin(request):
     data = json.loads(request.body)
-    buyer = Buyer.objects.get(uuid=data['uuid'])
-    response = json.dumps({'valid': buyer.pin.check(data['pin'])})
-    return HttpResponse(response)
+    try:
+        buyer = Buyer.objects.get(uuid=data['uuid'])
+        result = buyer.pin.check(data['pin'])
+    except Buyer.DoesNotExist:
+        result = False
+    return HttpResponse(json.dumps({'valid': result}))
