59067b399388fe918879c727af518bdd8c690377
Andy McKay <amckay@mozilla.com>
2013-01-04 10:03:01 -0800

    site specific config (bug 807135)

---
+++ solitude/settings/sites/dev/db.py
@@ -0,0 +1,43 @@
+"""private_base will be populated from puppet and placed in this directory"""
+
+import logging
+
+import private_base as private
+
+
+ADMINS = ()
+
+DATABASES = {'default': {}}
+
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+HMAC_KEYS = private.HMAC_KEYS
+
+LOG_LEVEL = logging.DEBUG
+
+SECRET_KEY = private.SECRET_KEY
+
+SENTRY_DSN = private.SENTRY_DSN
+
+STATSD_HOST = private.STATSD_HOST
+STATSD_PORT = private.STATSD_PORT
+STATSD_PREFIX = private.STATSD_PREFIX
+
+SYSLOG_TAG = 'http_app_payments_dev'
+TEMPLATE_DEBUG = DEBUG
+
+# Solitude specific settings.
+AES_KEYS = {}
+
+CLEANSED_SETTINGS_ACCESS = True
+CLIENT_JWT_KEYS = private.CLIENT_JWT_KEYS
+
+PAYPAL_APP_ID = private.PAYPAL_APP_ID
+PAYPAL_AUTH = private.PAYPAL_AUTH
+PAYPAL_CHAINS = private.PAYPAL_CHAINS
+PAYPAL_URL_WHITELIST = ('https://marketplace-dev.allizom.org',)
+PAYPAL_USE_SANDBOX = True
+
+BANGO_ENV = 'test'
+BANGO_AUTH = private.BANGO_AUTH
---
+++ solitude/settings/sites/dev/proxy.py
@@ -0,0 +1,45 @@
+"""private_base will be populated from puppet and placed in this directory"""
+
+import logging
+
+import dj_database_url
+
+import private_base as private
+
+
+ADMINS = ()
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.parse(private.DATABASES_DEFAULT_URL)
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+DATABASES['default']['OPTIONS'] = {'init_command': 'SET storage_engine=InnoDB'}
+
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+HMAC_KEYS = private.HMAC_KEYS
+
+LOG_LEVEL = logging.DEBUG
+
+SECRET_KEY = private.SECRET_KEY
+
+SENTRY_DSN = private.SENTRY_DSN
+
+STATSD_HOST = private.STATSD_HOST
+STATSD_PORT = private.STATSD_PORT
+STATSD_PREFIX = private.STATSD_PREFIX
+
+SYSLOG_TAG = 'http_app_payments_dev'
+TEMPLATE_DEBUG = DEBUG
+
+# Solitude specific settings.
+AES_KEYS = private.AES_KEYS
+
+CLEANSED_SETTINGS_ACCESS = True
+CLIENT_JWT_KEYS = private.CLIENT_JWT_KEYS
+
+PAYPAL_PROXY = private.PAYPAL_PROXY
+PAYPAL_URL_WHITELIST = ('https://marketplace-dev.allizom.org',)
+
+BANGO_ENV = 'test'
+BANGO_AUTH = private.BANGO_AUTH
