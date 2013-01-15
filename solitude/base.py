756a5819b7afffdf38d7c03a9d6161b9c8e76723
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-01-15 15:48:37 -0800

    pep8

--- solitude/base.py
+++ solitude/base.py
@@ -160,13 +160,13 @@ def log_cef(msg, request, **kw):
     g = functools.partial(getattr, settings)
     severity = kw.get('severity', g('CEF_DEFAULT_SEVERITY', 5))
     cef_kw = {'msg': msg, 'signature': request.get_full_path(),
-            'config': {
-                'cef.product': 'Solitude',
-                'cef.vendor': g('CEF_VENDOR', 'Mozilla'),
-                'cef.version': g('CEF_VERSION', '0'),
-                'cef.device_version': g('CEF_DEVICE_VERSION', '0'),
-                'cef.file': g('CEF_FILE', 'syslog'),
-            }
+              'config': {
+                  'cef.product': 'Solitude',
+                  'cef.vendor': g('CEF_VENDOR', 'Mozilla'),
+                  'cef.version': g('CEF_VERSION', '0'),
+                  'cef.device_version': g('CEF_DEVICE_VERSION', '0'),
+                  'cef.file': g('CEF_FILE', 'syslog'),
+              }
         }
     _log_cef(msg, severity, request.META.copy(), **cef_kw)
 
