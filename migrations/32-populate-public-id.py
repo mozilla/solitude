c04c4c6f551c6ed633cdd03a1990c51e2b342382
Andy McKay <amckay@mozilla.com>
2013-03-12 11:00:02 -0700

    cope with new databases

--- migrations/32-populate-public-id.py
+++ migrations/32-populate-public-id.py
@@ -1,9 +1,17 @@
 import uuid
 
+from django.db.utils import DatabaseError
+
 from lib.sellers.models import SellerProduct
 
 
 def run():
-    for seller in SellerProduct.objects.all():
-        seller.public_id = str(uuid.uuid4())
-        seller.save()
+    try:
+        for seller in SellerProduct.objects.all():
+            seller.public_id = str(uuid.uuid4())
+            seller.save()
+    except DatabaseError, exc:
+        if "Unknown column 'seller_product.access'" in str(exc):
+            # We don't need to worry about this migration then.
+            return
+        raise
