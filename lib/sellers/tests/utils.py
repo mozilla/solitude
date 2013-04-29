c7f97c39504416b0ae85a158680563f41bc072fc
Andy McKay <amckay@mozilla.com>
2013-04-29 14:33:35 -0700

    add in public_id

--- lib/sellers/tests/utils.py
+++ lib/sellers/tests/utils.py
@@ -3,6 +3,7 @@ from lib.sellers.models import Seller, SellerPaypal, SellerProduct
 
 def make_seller_paypal(uuid):
     seller = Seller.objects.create(uuid=uuid)
-    product = SellerProduct.objects.create(seller=seller, external_id='xyz')
+    product = SellerProduct.objects.create(seller=seller, external_id='xyz',
+        public_id='some:id')
     paypal = SellerPaypal.objects.create(seller=seller)
     return seller, paypal, product
