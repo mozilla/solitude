c9b7ab28845ab1245192d525cd15cbdff6893a60
Andy McKay <amckay@mozilla.com>
2013-05-28 16:37:24 -0700

    Revert "debug logging (bug 876662)"
    This reverts commit 8e7e7de4cf04f2434c90b972a019ccd27b99e550.

--- lib/bango/resources/billing.py
+++ lib/bango/resources/billing.py
@@ -93,7 +93,6 @@ class CreateBillingConfigurationResource(Resource):
             config.BillingConfigurationOption.append(opt)
 
         data['configurationOptions'] = config
-        log.debug('CreateBillingConfiguration: {0}'.format(data))
         resp = self.client('CreateBillingConfiguration', data)
         bundle.data = {'responseCode': resp.responseCode,
                        'responseMessage': resp.responseMessage,
