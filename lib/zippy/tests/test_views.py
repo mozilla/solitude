c96b1126f2ef4ff01abfa74029ca0d984a6b1b88
David Larlet <david@larlet.fr>
2013-11-14 11:44:32 +0100

    Adding support for creating zippy products (bug 932583)

--- lib/zippy/tests/test_views.py
+++ lib/zippy/tests/test_views.py
@@ -32,69 +32,69 @@ class TestAPIView(TestCase):
                                      resource_name='sellers').status_code, 404)
 
 
-class TestListViews(TestCase):
+class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.url = reverse('zippy.api_view',
-                           args=['reference', 'sellers'])
+        self.seller_uuid = 'zippy-seller-uuid'
+        self.product_uuid = 'zippy-product-uuid'
 
-    def test_retrieve_sellers_empty(self):
-        resp = self.client.get(self.url)
-        ok_(resp['Content-Type'].startswith('application/json'))
+    def url_list(self, resource_name):
+        return reverse('zippy.api_view', args=['reference', resource_name])
 
-    def test_create_seller(self):
-        uuid = 'zippy-uuid'
-        seller = {
-            'uuid': uuid,
-            'status': 'ACTIVE',
-            'name': 'John',
-            'email': 'jdoe@example.org',
-        }
-        resp = self.client.post(self.url, seller)
-        seller.update({
-            'resource_pk': '1',
-            'resource_uri': '/sellers/1',
-        })
-        eq_(json.loads(resp.content), seller)
-
-
-class TestItemViews(TestCase):
-
-    def setUp(self):
-        self.client = Client()
-        self.uuid = 'zippy-uuid'
-        self.url_list = reverse('zippy.api_view',
-                                args=['reference', 'sellers'])
-
-    def url(self, pk):
-        return reverse('zippy.api_view',
-                       args=['reference', 'sellers', pk])
+    def url_item(self, resource_name, pk):
+        return reverse('zippy.api_view', args=['reference', resource_name, pk])
 
     def create_seller(self):
         seller = {
-            'uuid': self.uuid,
+            'uuid': self.seller_uuid,
             'status': 'ACTIVE',
             'name': 'John',
             'email': 'jdoe@example.org',
         }
-        self.client.post(self.url_list, seller)
+        self.client.post(self.url_list('sellers'), seller)
         seller.update({
             'resource_pk': '1',
             'resource_uri': '/sellers/1',
         })
         return seller
 
+    def delete_seller(self):
+        self.client.delete(self.url_item('sellers', self.seller_uuid))
+
+
+class TestSellerViews(TestViews):
+
+    def test_retrieve_sellers_empty(self):
+        self.delete_seller()
+        resp = self.client.get(self.url_list('sellers'))
+        ok_(resp['Content-Type'].startswith('application/json'))
+        eq_(json.loads(resp.content), [])
+
+    def test_create_seller(self):
+        seller = {
+            'uuid': self.seller_uuid,
+            'status': 'ACTIVE',
+            'name': 'John',
+            'email': 'jdoe@example.org',
+        }
+        resp = self.client.post(self.url_list('sellers'), seller)
+        seller.update({
+            'resource_pk': '1',
+            'resource_uri': '/sellers/1',
+        })
+        eq_(json.loads(resp.content), seller)
+
     def test_retrieve_seller(self):
         seller = self.create_seller()
-        resp = self.client.get(self.url(self.uuid))
+        resp = self.client.get(self.url_item('sellers', self.seller_uuid))
         eq_(json.loads(resp.content), seller)
         ok_(resp['Content-Type'].startswith('application/json'))
 
     def test_update_seller(self):
         seller = self.create_seller()
         new_name = 'Jack'
-        resp = self.client.put(self.url(self.uuid),
+        resp = self.client.put(self.url_item('sellers', self.seller_uuid),
                                json.dumps({'name': new_name}),
                                content_type='application/json')
         seller.update({ 'name': new_name })
@@ -102,5 +102,21 @@ class TestItemViews(TestCase):
 
     def test_delete_seller(self):
         self.create_seller()
-        resp = self.client.delete(self.url(self.uuid))
+        resp = self.client.delete(self.url_item('sellers', self.seller_uuid))
         eq_(resp.status_code, 200)
+
+
+class TestProductViews(TestViews):
+
+    def test_create_product(self):
+        seller = self.create_seller()
+        product = {
+            'seller_id': seller['resource_pk'],
+            'external_id': self.product_uuid,
+        }
+        resp = self.client.post(self.url_list('products'), product)
+        product.update({
+            'resource_pk': '1',
+            'resource_uri': '/products/1',
+        })
+        eq_(json.loads(resp.content), product)
