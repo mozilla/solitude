26244b5e39f0a4b4dc5fceb1ada67056c3b6d013
Andy McKay <amckay@mozilla.com>
2013-11-20 14:42:24 -0800

    make zippy basic use solitude

--- samples/bango-basic.py
+++ samples/bango-basic.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-import json
-import pprint
+import functools
 import sys
 import urllib
 import uuid
 
-import requests
+import lib
 
 try:
     # Read the root from the command line, rather than hard coding.
@@ -14,31 +13,8 @@ except:
     root = 'http://localhost:8001'
 
 
-def call(url, method, data):
-    method = getattr(requests, method)
-    url = root + url
-    print 'Calling url:', url
-    print 'Request data:'
-    pprint.pprint(data)
-    data = json.dumps(data)
-    result = method(url, data=data,
-                    headers={'content-type': 'application/json'})
-    print 'Status code:', result.status_code
-    if result.status_code not in (200, 201, 202, 204):
-        print 'Error:', result.content
-        sys.exit()
-
-    if result.content:
-        print 'Response data:'
-        data = result.json
-        pprint.pprint(data)
-        print
-        return data
-
-    print
-
-
 uid = str(uuid.uuid4())
+call = functools.partial(lib.call, root)
 
 print 'Creating for:', uid
 print 'Creating seller.'
