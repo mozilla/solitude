b1bcd500c047c10698cf2d2e26594b6166a4ae43
Jeremiah Orem <oremj@oremj.com>
2014-03-07 15:22:49 -0800

    settings: add bangostage environment

---
+++ solitude/settings/sites/bangostage/db.py
@@ -0,0 +1,62 @@
+"""private_base will be populated from puppet and placed in this directory"""
+
+import logging
+
+import dj_database_url
+
+import private_base as private
+
+from solitude.settings import base
+from django_sha2 import get_password_hashers
+
+
+ADMINS = ()
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.parse(private.DATABASES_DEFAULT_URL)
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+DATABASES['default']['OPTIONS'] = {'init_command': 'SET storage_engine=InnoDB'}
+
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+HMAC_KEYS = private.HMAC_KEYS
+
+PASSWORD_HASHERS = get_password_hashers(base.BASE_PASSWORD_HASHERS, HMAC_KEYS)
+
+LOG_LEVEL = logging.DEBUG
+
+SECRET_KEY = private.SECRET_KEY
+
+SENTRY_DSN = private.SENTRY_DSN
+
+STATSD_HOST = private.STATSD_HOST
+STATSD_PORT = private.STATSD_PORT
+STATSD_PREFIX = private.STATSD_PREFIX
+
+SYSLOG_TAG = 'http_app_payments_bango_stage'
+TEMPLATE_DEBUG = DEBUG
+
+# Solitude specific settings.
+AES_KEYS = private.AES_KEYS
+CLIENT_OAUTH_KEYS = private.CLIENT_OAUTH_KEYS
+
+PAYPAL_PROXY = private.PAYPAL_PROXY
+PAYPAL_URL_WHITELIST = ('https://marketplace.allizom.org',)
+
+BANGO_BASIC_AUTH = private.BANGO_BASIC_AUTH
+BANGO_ENV = 'prod'
+BANGO_INSERT_STAGE = 'FROM STAGE '
+BANGO_PROXY = private.BANGO_PROXY
+BANGO_NOTIFICATION_URL = (
+    'https://marketplace.allizom.org/mozpay/bango/notification')
+
+SITE_URL = 'https://payments-bango.allizom.org'
+
+S3_AUTH = {'key': private.S3_AUTH_KEY, 'secret': private.S3_AUTH_SECRET}
+S3_BUCKET = private.S3_BUCKET
+
+REQUIRE_OAUTH = True
+
+NEWRELIC_INI = '/etc/newrelic.d/payments-bango.allizom.org.ini'
---
+++ solitude/settings/sites/bangostage/proxy.py
@@ -0,0 +1,51 @@
+"""private_base will be populated from puppet and placed in this directory"""
+
+import logging
+
+import private_base as private
+
+from solitude.settings import base
+from django_sha2 import get_password_hashers
+
+
+ADMINS = ()
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {'default': {}}
+
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+HMAC_KEYS = private.HMAC_KEYS
+
+PASSWORD_HASHERS = get_password_hashers(base.BASE_PASSWORD_HASHERS, HMAC_KEYS)
+
+LOG_LEVEL = logging.DEBUG
+
+SECRET_KEY = private.SECRET_KEY
+
+SENTRY_DSN = private.SENTRY_DSN
+
+STATSD_HOST = private.STATSD_HOST
+STATSD_PORT = private.STATSD_PORT
+STATSD_PREFIX = private.STATSD_PREFIX
+
+SYSLOG_TAG = 'http_app_payments_bango_stage'
+TEMPLATE_DEBUG = DEBUG
+
+# Solitude specific settings.
+AES_KEYS = {}
+
+CLEANSED_SETTINGS_ACCESS = True
+CLIENT_JWT_KEYS = private.CLIENT_JWT_KEYS
+
+PAYPAL_APP_ID = private.PAYPAL_APP_ID
+PAYPAL_AUTH = private.PAYPAL_AUTH
+PAYPAL_CHAINS = private.PAYPAL_CHAINS
+PAYPAL_URL_WHITELIST = ('https://marketplace.allizom.org',)
+PAYPAL_USE_SANDBOX = True
+
+BANGO_ENV = 'prod'
+BANGO_AUTH = private.BANGO_AUTH
+
+NEWRELIC_INI = '/etc/newrelic.d/payments-bango-proxy.allizom.org.ini'
