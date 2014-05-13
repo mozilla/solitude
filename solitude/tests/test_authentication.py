41d68ae87fc288490654d432b95ed4c5455ddb91
Andy McKay <amckay@mozilla.com>
2014-05-13 11:38:41 -0700

    create a whitelist of urls (bug 1009710)

--- solitude/tests/test_authentication.py
+++ solitude/tests/test_authentication.py
@@ -5,8 +5,10 @@ import test_utils
 from curling.lib import sign_request
 from mock import patch
 from nose.tools import eq_, ok_
+from rest_framework.exceptions import AuthenticationFailed
 
-from solitude.authentication import Consumer, OAuthAuthentication
+from solitude.authentication import (Consumer, OAuthAuthentication,
+                                     RestOAuthAuthentication)
 
 keys = {'foo': 'bar'}
 keys_dict = {'key': 'foo', 'secret': 'bar'}
@@ -22,12 +24,22 @@ class TestAuthentication(test_utils.TestCase):
 
     def test_not_required(self):
         req = self.factory.get('/')
-        with self.settings(REQUIRE_OAUTH=False):
+        with self.settings(REQUIRE_OAUTH=False, SKIP_OAUTH=[]):
             ok_(self.authentication.is_authenticated(req))
 
     def test_required(self):
         req = self.factory.get('/')
-        with self.settings(REQUIRE_OAUTH=True):
+        with self.settings(REQUIRE_OAUTH=True, SKIP_OAUTH=[]):
+            ok_(not self.authentication.is_authenticated(req))
+
+    def test_skip(self):
+        req = self.factory.get('/skip-oauth/')
+        with self.settings(REQUIRE_OAUTH=True, SKIP_OAUTH=['/skip-oauth/']):
+            ok_(self.authentication.is_authenticated(req))
+
+    def test_not_skip(self):
+        req = self.factory.get('/require-oauth/')
+        with self.settings(REQUIRE_OAUTH=True, SKIP_OAUTH=['/skip-oauth/']):
             ok_(not self.authentication.is_authenticated(req))
 
     def setup_authorization(self, keys):
@@ -39,7 +51,7 @@ class TestAuthentication(test_utils.TestCase):
     def test_signed(self):
         authorization = self.setup_authorization(keys_dict)
         req = self.factory.get('/', HTTP_AUTHORIZATION=authorization)
-        with self.settings(REQUIRE_OAUTH=True):
+        with self.settings(REQUIRE_OAUTH=True, SKIP_OAUTH=[]):
             ok_(self.authentication.is_authenticated(req))
             eq_(req.OAUTH_KEY, 'foo')
 
@@ -48,6 +60,25 @@ class TestAuthentication(test_utils.TestCase):
         keys_['secret'] = 'baz'
         authorization = self.setup_authorization(keys_)
         req = self.factory.get('/foo/', HTTP_AUTHORIZATION=authorization)
-        with self.settings(REQUIRE_OAUTH=True):
+        with self.settings(REQUIRE_OAUTH=True, SKIP_OAUTH=[]):
             ok_(not self.authentication.is_authenticated(req))
             eq_(req.OAUTH_KEY, None)
+
+
+@patch.object(settings, 'CLIENT_OAUTH_KEYS', keys)
+class TestDRFAuthentication(test_utils.TestCase):
+
+    def setUp(self):
+        self.authentication = RestOAuthAuthentication()
+        self.factory = RequestFactory()
+        self.consumer = Consumer(*keys.items()[0])
+
+    def test_skip(self):
+        req = self.factory.get('/skip-oauth/')
+        with self.settings(REQUIRE_OAUTH=True, SKIP_OAUTH=['/skip-oauth/']):
+            ok_(self.authentication.authenticate(req))
+
+    def test_not_skip(self):
+        req = self.factory.get('/require-oauth/')
+        with self.settings(REQUIRE_OAUTH=True, SKIP_OAUTH=['/skip-oauth/']):
+            eq_(self.authentication.authenticate(req), AuthenticationFailed)
