ebdc31d987d20d34c22603cb82b38fc473d57bbd
Andy McKay <amckay@mozilla.com>
2014-07-04 12:57:19 -0700

    fix Can't write; duplicate key in table '#sql-7fb2_1e48e71'

--- migrations/47-add-product-reference.sql
+++ migrations/47-add-product-reference.sql
@@ -5,9 +5,9 @@ CREATE TABLE `seller_reference` (
     `counter` bigint,
     `seller_id` int(11) unsigned NOT NULL UNIQUE,
     `merchant_id` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-ALTER TABLE `seller_reference` ADD CONSTRAINT `seller_id_refs_id_d3a72381` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`id`);
+ALTER TABLE `seller_reference` ADD CONSTRAINT `seller_id_refs_id_reference` FOREIGN KEY (`seller_id`) REFERENCES `seller` (`id`);
 
 CREATE TABLE `seller_product_reference` (
     `id` int(11) unsigned AUTO_INCREMENT NOT NULL PRIMARY KEY,
