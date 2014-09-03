6bed11fdde3a2b2977562af17c192da8d28625b1
Jason Thomas <jason@lithiumfox.com>
2014-09-03 16:11:02 -0400

    Add settings for altdev.

---
+++ solitude/settings/sites/altdev/db.py
@@ -0,0 +1,78 @@
+"""private_base will be populated from puppet and placed in this directory"""
+
+import logging
+
+import dj_database_url
+
+import private_base as private
+
+from solitude.settings import base
+from django_sha2 import get_password_hashers
+
+
+ADMINS = ()
+ALLOWED_HOSTS = ['payments-altdev.allizom.org', 'localhost']
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.parse(private.DATABASES_DEFAULT_URL)
+DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+DATABASES['default']['OPTIONS'] = {'init_command': 'SET storage_engine=InnoDB'}
+
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+HMAC_KEYS = private.HMAC_KEYS
+
+PASSWORD_HASHERS = get_password_hashers(base.BASE_PASSWORD_HASHERS, HMAC_KEYS)
+
+LOG_LEVEL = logging.DEBUG
+
+SECRET_KEY = private.SECRET_KEY
+
+SENTRY_DSN = private.SENTRY_DSN
+
+STATSD_HOST = private.STATSD_HOST
+STATSD_PORT = private.STATSD_PORT
+STATSD_PREFIX = private.STATSD_PREFIX
+
+SYSLOG_TAG = 'http_app_payments_altdev'
+TEMPLATE_DEBUG = DEBUG
+
+# Solitude specific settings.
+AES_KEYS = private.AES_KEYS
+
+CLEANSED_SETTINGS_ACCESS = True
+CLIENT_OAUTH_KEYS = private.CLIENT_OAUTH_KEYS
+
+SITE_URL = 'https://payments-altdev.allizom.org'
+
+S3_AUTH = {'key': private.S3_AUTH_KEY, 'secret': private.S3_AUTH_SECRET}
+S3_BUCKET = private.S3_BUCKET
+
+NEWRELIC_INI = None
+
+# Below is configuration of payment providers.
+
+ZIPPY_PROXY = 'https://payments-proxy-altdev.allizom.org/proxy/provider'
+ZIPPY_CONFIGURATION = {
+    'reference': {
+        'url': 'https://zippy-dev.allizom.org'
+    },
+    'boku': {
+        'url': base.BOKU_API_DOMAIN
+    }
+}
+
+PAYPAL_PROXY = private.PAYPAL_PROXY
+PAYPAL_URL_WHITELIST = ('https://marketplace-altdev.allizom.org',)
+
+BANGO_BASIC_AUTH = private.BANGO_BASIC_AUTH
+BANGO_FAKE_REFUNDS = True
+BANGO_PROXY = private.BANGO_PROXY
+BANGO_NOTIFICATION_URL = (
+    'https://marketplace-altdev.allizom.org/mozpay/bango/notification')
+
+
+BOKU_PROXY = ZIPPY_PROXY
+
+NOSE_PLUGINS = []
---
+++ solitude/settings/sites/altdev/proxy.py
@@ -0,0 +1,67 @@
+"""private_base will be populated from puppet and placed in this directory"""
+
+import logging
+
+import private_base as private
+
+from solitude.settings import base
+from django_sha2 import get_password_hashers
+
+
+ADMINS = ()
+ALLOWED_HOSTS = ['payments-proxy-altdev.allizom.org']
+
+DATABASES = {'default': {}}
+
+DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = False
+
+HMAC_KEYS = private.HMAC_KEYS
+
+PASSWORD_HASHERS = get_password_hashers(base.BASE_PASSWORD_HASHERS, HMAC_KEYS)
+
+LOG_LEVEL = logging.DEBUG
+
+SECRET_KEY = private.SECRET_KEY
+
+SENTRY_DSN = private.SENTRY_DSN
+
+STATSD_HOST = private.STATSD_HOST
+STATSD_PORT = private.STATSD_PORT
+STATSD_PREFIX = private.STATSD_PREFIX
+
+SYSLOG_TAG = 'http_app_payments_altdev'
+TEMPLATE_DEBUG = DEBUG
+
+# Solitude specific settings.
+AES_KEYS = {}
+
+CLEANSED_SETTINGS_ACCESS = True
+CLIENT_JWT_KEYS = private.CLIENT_JWT_KEYS
+
+NEWRELIC_INI = None
+
+# Below is configuration of payment providers.
+
+PAYPAL_APP_ID = private.PAYPAL_APP_ID
+PAYPAL_AUTH = private.PAYPAL_AUTH
+PAYPAL_CHAINS = private.PAYPAL_CHAINS
+PAYPAL_URL_WHITELIST = ('https://marketplace-altdev.allizom.org',)
+PAYPAL_USE_SANDBOX = True
+
+BANGO_ENV = 'test'
+BANGO_AUTH = private.BANGO_AUTH
+
+ZIPPY_CONFIGURATION = {
+    'reference': {
+        'url': 'https://zippy-dev.allizom.org',
+        'auth': {'key': private.ZIPPY_PAAS_KEY,
+                 'secret': private.ZIPPY_PAAS_SECRET,
+                 'realm': 'zippy-dev.allizom.org'}
+    },
+    'boku': {
+        'url': base.BOKU_API_DOMAIN,
+    }
+}
+
+NOSE_PLUGINS = []
