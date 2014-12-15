d9c9b9d11a30006efa51cb1cce83bdf81c070c3a
Andy McKay <amckay@mozilla.com>
2014-12-15 15:15:02 -0800

    move settings_test to solitude.settings.test

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 # Edit this if necessary or override the variable in your environment.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solitude.settings')
 if sys.argv[1] == 'test':
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'settings_test'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'solitude.settings.test'
 
 # This constant moved in Django 1.5 but it is used by Tastypie. When that
 # gets updated we can remove this.
