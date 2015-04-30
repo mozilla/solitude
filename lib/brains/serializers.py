99501acc397399077ce8943e979838f01bc86beb
Andy McKay <amckay@mozilla.com>
2015-04-30 14:25:46 -0700

    add in some docs on the braintree buyer object

--- lib/brains/serializers.py
+++ lib/brains/serializers.py
@@ -1,15 +1,21 @@
+from django.core.urlresolvers import reverse
+
 from rest_framework import serializers
 
 from lib.brains.models import BraintreeBuyer
+from solitude.base import BaseSerializer
 from solitude.related_fields import PathRelatedField
 
 
-class BraintreeBuyerSerializer(serializers.ModelSerializer):
+class BraintreeBuyerSerializer(BaseSerializer):
     buyer = PathRelatedField(view_name='braintree:buyer-detail')
 
     class Meta:
         model = BraintreeBuyer
         read_only = ['braintree_id']
+
+    def resource_uri(self, pk):
+        return reverse('braintree:buyer-detail', kwargs={'pk': pk})
 
 
 class BraintreeSerializer(serializers.Serializer):
