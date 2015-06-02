180a484400933dfc0980bd7f28552423df4f32bc
Andy McKay <amckay@mozilla.com>
2015-06-02 16:48:51 -0700

    remove errant settings

--- lib/brains/views/webhook.py
+++ lib/brains/views/webhook.py
@@ -1,8 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from django.conf import settings
-
 from lib.brains.client import get_client
 from lib.brains.forms import WebhookParseForm, WebhookVerifyForm
 from solitude.errors import FormError
