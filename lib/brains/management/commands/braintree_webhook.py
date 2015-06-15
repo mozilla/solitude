6920d10bc244da84759c7868b0b823a3aa604ec1
Andy McKay <amckay@mozilla.com>
2015-06-15 12:58:32 -0700

    fake webhooks

---
+++ lib/brains/management/commands/braintree_webhook.py
@@ -0,0 +1,142 @@
+import base64
+import os
+from datetime import datetime, timedelta
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+
+import braintree
+import requests
+from braintree.util.crypto import Crypto
+
+from lib.brains.models import BraintreeSubscription
+from lib.brains.tests.samples import webhooks
+from payments_config import products
+from solitude.logger import getLogger
+
+log = getLogger('s.brains.management')
+valid_kinds = ['subscription_charged_successfully']
+
+
+class Command(BaseCommand):
+
+    """
+    This is a crude way to generate and test webhook notifications.
+
+    It does this by grabbing a sample request from Braintree and then
+    reformatting it with some local data. There are some inherent problems
+    with this:
+    * the XML format might change
+    * the data is not complete, I've changed the bits I care about, make
+      sure the data is set correctly before you rely on it
+    * this code will probably go away once we've got decent end to end
+      testing in place
+
+    If that doesn't deter you...
+
+        braintree_webhook
+                --parse=subscription_charged_successfully
+                --subscription_id=14
+
+    Will send this fake and hacky Braintree webhook to that end point.
+    You'll need to get subscription_id by looking at the solitude table
+    `braintree_subscription`.
+    """
+
+    help = ('Generates fakes webhooks sent by braintree for '
+            'testing and development. Caveat emptor.')
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--verify',
+            dest='verify',
+            action='store_true',
+            help=('Send the verify request.')
+        ),
+        make_option(
+            '--parse',
+            dest='parse',
+            help=('Kind of message to send.')
+        ),
+        make_option(
+            '--server',
+            dest='server',
+            action='store_true',
+            default='http://pay.dev:8000/api/braintree/webhook/',
+            help=('URL of payments-service server.')
+        ),
+        make_option(
+            '--subscription_id',
+            dest='subscription_id',
+            help=('Primary key of the subscription in solitude.')
+        )
+    )
+
+    def verify(self, url):
+        res = requests.get(url, params={'bt_challenge': 'something'})
+        res.raise_for_status()
+        # Ideally we could do more to verify the challenge is correct.
+        if res.status_code != 200:
+            raise CommandError(
+                'Server did not return a 200 response, got: {}'
+                .format(res.status_code)
+            )
+
+    def webhook(self, url, kind, sub):
+        data = {
+            'merchant_account_id': self.merchant,
+            'sub': sub,
+            'plan_id': sub.seller_product.public_id,
+            'product': products[sub.seller_product.public_id],
+            'now': datetime.today(),
+            'timestamp': datetime.today().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            # Braintree doesn't assume months are 30 days long.
+            'paid': datetime.today() + timedelta(days=29),
+            'next': datetime.today() + timedelta(days=30),
+        }
+        xml_blob = getattr(webhooks, kind)
+        xml_formatted = xml_blob.format(**data)
+        payload = base64.encodestring(xml_formatted)
+        res = requests.post(url, data={
+            'bt_signature':
+                self.public + '|' +
+                Crypto.sha1_hmac_hash(self.private, payload),
+            'bt_payload': payload
+        })
+        res.raise_for_status()
+        if res.status_code != 200:
+            raise CommandError(
+                'Server did not return a 200 response, got: {}'
+                .format(res.status_code)
+            )
+
+    def handle(self, *args, **options):
+        # These environment variables are populated by Docker.
+        # This script will not run out of Docker unless you
+        # populate these variables.
+        self.merchant = os.environ['BRAINTREE_MERCHANT_ID']
+        self.public = os.environ['SOLITUDE_AUTH_ENV_BRAINTREE_PUBLIC_KEY']
+        self.private = os.environ['SOLITUDE_AUTH_ENV_BRAINTREE_PRIVATE_KEY']
+
+        braintree.Configuration.configure(
+            'sandbox', self.merchant, self.public, self.private)
+
+        if options['verify']:
+            log.info('Sending verify command')
+            return self.verify(options['server'])
+
+        if options['parse']:
+            subscription = BraintreeSubscription.objects.get(
+                id=options['subscription_id'])
+            if options['parse'] not in valid_kinds:
+                raise CommandError(
+                    'Not a valid kind of webhook: {} must be one of: {}'
+                    .format(options['kind'], ', '.join(valid_kinds))
+                )
+
+            log.info('Sending parse command for subscription: {}'
+                     .format(subscription.id))
+            return self.webhook(
+                options['server'],
+                options['parse'],
+                subscription
+            )
---
+++ lib/brains/management/commands/samples/webhooks.py
@@ -0,0 +1,195 @@
+subscription_charged_successfully = """<?xml version="1.0" encoding="UTF-8"?>
+<notification>
+  <kind>subscription_charged_successfully</kind>
+  <timestamp type="datetime">2015-06-15T18:34:41Z</timestamp>
+  <subject>
+    <subscription>
+      <add-ons type="array"/>
+      <balance>0.00</balance>
+      <billing-day-of-month type="integer">{now.day}</billing-day-of-month>
+      <billing-period-end-date type="date">{next:%Y-%m-%d}</billing-period-end-date>
+      <billing-period-start-date type="date">{now:%Y-%m-%d}</billing-period-start-date>
+      <created-at type="datetime">{timestamp}</created-at>
+      <updated-at type="datetime">{timestamp}</updated-at>
+      <current-billing-cycle type="integer">1</current-billing-cycle>
+      <days-past-due nil="true"/>
+      <discounts type="array"/>
+      <failure-count type="integer">0</failure-count>
+      <first-billing-date type="date">{now:%Y-%m-%d}</first-billing-date>
+      <id>{sub.provider_id}</id>
+      <merchant-account-id>{merchant_account_id}</merchant-account-id>
+      <never-expires type="boolean">true</never-expires>
+      <next-bill-amount>{product.amount}</next-bill-amount>
+      <next-billing-period-amount>{product.amount}</next-billing-period-amount>
+      <next-billing-date type="date">{next:%Y-%m-%d}</next-billing-date>
+      <number-of-billing-cycles nil="true"/>
+      <paid-through-date type="date">{paid:%Y-%m-%d}</paid-through-date>
+      <payment-method-token>7js9mb</payment-method-token>
+      <plan-id>{plan_id}</plan-id>
+      <price>{product.amount}</price>
+      <status>Active</status>
+      <trial-duration nil="true"/>
+      <trial-duration-unit>day</trial-duration-unit>
+      <trial-period type="boolean">false</trial-period>
+      <descriptor>
+        <name>Mozilla*product</name>
+        <phone nil="true"/>
+        <url>mozilla.org</url>
+      </descriptor>
+      <transactions type="array">
+        <transaction>
+          <id>6zdhry</id>
+          <status>submitted_for_settlement</status>
+          <type>sale</type>
+          <currency-iso-code>{product.currency}</currency-iso-code>
+          <amount>{product.amount}</amount>
+          <merchant-account-id>{merchant_account_id}</merchant-account-id>
+          <order-id nil="true"/>
+          <created-at type="datetime">{timestamp}</created-at>
+          <updated-at type="datetime">{timestamp}</updated-at>
+          <customer>
+            <id>86199926</id>
+            <first-name nil="true"/>
+            <last-name nil="true"/>
+            <company nil="true"/>
+            <email nil="true"/>
+            <website nil="true"/>
+            <phone nil="true"/>
+            <fax nil="true"/>
+          </customer>
+          <billing>
+            <id nil="true"/>
+            <first-name nil="true"/>
+            <last-name nil="true"/>
+            <company nil="true"/>
+            <street-address nil="true"/>
+            <extended-address nil="true"/>
+            <locality nil="true"/>
+            <region nil="true"/>
+            <postal-code nil="true"/>
+            <country-name nil="true"/>
+            <country-code-alpha2 nil="true"/>
+            <country-code-alpha3 nil="true"/>
+            <country-code-numeric nil="true"/>
+          </billing>
+          <refund-id nil="true"/>
+          <refund-ids type="array"/>
+          <refunded-transaction-id nil="true"/>
+          <settlement-batch-id nil="true"/>
+          <shipping>
+            <id nil="true"/>
+            <first-name nil="true"/>
+            <last-name nil="true"/>
+            <company nil="true"/>
+            <street-address nil="true"/>
+            <extended-address nil="true"/>
+            <locality nil="true"/>
+            <region nil="true"/>
+            <postal-code nil="true"/>
+            <country-name nil="true"/>
+            <country-code-alpha2 nil="true"/>
+            <country-code-alpha3 nil="true"/>
+            <country-code-numeric nil="true"/>
+          </shipping>
+          <custom-fields/>
+          <avs-error-response-code nil="true"/>
+          <avs-postal-code-response-code>I</avs-postal-code-response-code>
+          <avs-street-address-response-code>I</avs-street-address-response-code>
+          <cvv-response-code>I</cvv-response-code>
+          <gateway-rejection-reason nil="true"/>
+          <processor-authorization-code>KQPBDL</processor-authorization-code>
+          <processor-response-code>1000</processor-response-code>
+          <processor-response-text>Approved</processor-response-text>
+          <additional-processor-response nil="true"/>
+          <voice-referral-number nil="true"/>
+          <purchase-order-number nil="true"/>
+          <tax-amount nil="true"/>
+          <tax-exempt type="boolean">false</tax-exempt>
+          <credit-card>
+            <token>7js9mb</token>
+            <bin>411111</bin>
+            <last-4>1111</last-4>
+            <card-type>Visa</card-type>
+            <expiration-month>12</expiration-month>
+            <expiration-year>2015</expiration-year>
+            <customer-location>US</customer-location>
+            <cardholder-name nil="true"/>
+            <image-url>https://assets.braintreegateway.com/payment_method_logo/visa.png?environment=sandbox</image-url>
+            <unique-number-identifier>21ee4272998b10107aeee9d50d6fe1ae</unique-number-identifier>
+            <prepaid>Unknown</prepaid>
+            <healthcare>Unknown</healthcare>
+            <debit>Unknown</debit>
+            <durbin-regulated>Unknown</durbin-regulated>
+            <commercial>Unknown</commercial>
+            <payroll>Unknown</payroll>
+            <issuing-bank>Unknown</issuing-bank>
+            <country-of-issuance>Unknown</country-of-issuance>
+            <product-id>Unknown</product-id>
+            <venmo-sdk type="boolean">false</venmo-sdk>
+          </credit-card>
+          <status-history type="array">
+            <status-event>
+              <timestamp type="datetime">2015-06-15T18:34:40Z</timestamp>
+              <status>authorized</status>
+              <amount>5.00</amount>
+              <user>andymckay</user>
+              <transaction-source>recurring</transaction-source>
+            </status-event>
+            <status-event>
+              <timestamp type="datetime">2015-06-15T18:34:40Z</timestamp>
+              <status>submitted_for_settlement</status>
+              <amount>5.00</amount>
+              <user>andymckay</user>
+              <transaction-source>recurring</transaction-source>
+            </status-event>
+          </status-history>
+          <plan-id>mozilla-concrete-mortar</plan-id>
+          <subscription-id>fpps4w</subscription-id>
+          <subscription>
+            <billing-period-end-date type="date">2015-07-14</billing-period-end-date>
+            <billing-period-start-date type="date">2015-06-15</billing-period-start-date>
+          </subscription>
+          <add-ons type="array"/>
+          <discounts type="array"/>
+          <descriptor>
+            <name>Mozilla*product</name>
+            <phone nil="true"/>
+            <url>mozilla.org</url>
+          </descriptor>
+          <recurring type="boolean">true</recurring>
+          <channel nil="true"/>
+          <service-fee-amount nil="true"/>
+          <escrow-status nil="true"/>
+          <disbursement-details>
+            <disbursement-date nil="true"/>
+            <settlement-amount nil="true"/>
+            <settlement-currency-iso-code nil="true"/>
+            <settlement-currency-exchange-rate nil="true"/>
+            <funds-held nil="true"/>
+            <success nil="true"/>
+          </disbursement-details>
+          <disputes type="array"/>
+          <payment-instrument-type>credit_card</payment-instrument-type>
+          <processor-settlement-response-code></processor-settlement-response-code>
+          <processor-settlement-response-text></processor-settlement-response-text>
+          <risk-data>
+            <id>3CSZ08CD2ZY4</id>
+            <decision>Approve</decision>
+          </risk-data>
+          <three-d-secure-info nil="true"/>
+        </transaction>
+      </transactions>
+      <status-history type="array">
+        <status-event>
+          <timestamp type="datetime">2015-06-15T18:34:40Z</timestamp>
+          <status>Active</status>
+          <user>andymckay</user>
+          <subscription-source>api</subscription-source>
+          <balance>0.00</balance>
+          <price>5.00</price>
+        </status-event>
+      </status-history>
+    </subscription>
+  </subject>
+</notification>
+"""  # noqa
