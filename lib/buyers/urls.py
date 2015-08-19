2fb2fed8933a010677f29bbfbb181eb6abd5f702
Andy McKay <amckay@mozilla.com>
2015-08-19 13:41:23 -0700

    remove $ from urls so / or no / works

--- lib/buyers/urls.py
+++ lib/buyers/urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns(
     '',
     url(r'', include(router.urls)),
     url(r'^generic/buyer/(?P<pk>[\d]+)/close/$', views.close, name='close'),
-    url(r'^confirm_pin$', views.confirm_pin, name='confirm'),
-    url(r'^verify_pin$', views.verify_pin, name='verify'),
-    url(r'^reset_confirm_pin$', views.reset_confirm_pin,
+    url(r'^confirm_pin', views.confirm_pin, name='confirm'),
+    url(r'^verify_pin', views.verify_pin, name='verify'),
+    url(r'^reset_confirm_pin', views.reset_confirm_pin,
         name='reset_confirm_pin')
 )
